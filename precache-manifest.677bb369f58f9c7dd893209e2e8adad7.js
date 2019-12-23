9f259e22ef65f5e121cc748eee67d11cd0ee812b
daempasha <daempasha@gmail.com>
2019-12-23 21:13:00 +0000

    Updates

---
+++ precache-manifest.677bb369f58f9c7dd893209e2e8adad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5dedf8f00b3915e186475576e329da",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06f0f5408ac21e1b4ced",
+    "url": "/monsters-rolodex/static/css/main.6c3c4921.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06f0f5408ac21e1b4ced",
+    "url": "/monsters-rolodex/static/js/main.73de608e.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
